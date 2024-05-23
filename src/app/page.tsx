6545ad77214a5c7c996c6198cc60f752d528c1d5
steel-bucket <depnra1@gmail.com>
2024-05-23 21:02:10 +0530

    scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 const mockUrls = [
-    "https://utfs.io/f/bfdb9c42-cf07-4af7-ba93-b5f0276f619a-492wi6.jpg",
-    "https://utfs.io/f/f98a7327-a9b4-4329-9945-113cb710748c-g390hq.jpg",
+    "https://utfs.io/f/f476978c-039c-47b8-b55d-a2ac24c5cc58-1wb414.avif",
+    "https://utfs.io/f/d907478a-0e04-4e3a-8632-351da21c1dc8-bkvfhx.avif",
     "https://utfs.io/f/f0afd3bc-3355-44ad-9b29-e14cc5aaf44b-o0xo9b.jpg",
-    "https://utfs.io/f/69433c90-9d40-48d1-b77c-e0268809ced8-dfw51g.jpg"
+    "https://utfs.io/f/6f28ec06-d622-4d80-92c9-309c0ffa9b66-vto92j.avif"
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
@@ -18,9 +18,9 @@ export default function HomePage() {
         <main className="">
             <div className="flex flex-wrap gap-4">
                 {
-                    mockImages.map((image) => (
+                    [...mockImages,...mockImages,...mockImages,].map((image) => (
                         <div key={image.id} className="w-48">
-                            <img src={image.url} alt="image"/>
+                            <img src={image.url} />
                         </div>
                     ))
                 }
