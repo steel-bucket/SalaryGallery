45ec93968fe9d729c96dc46862c80d5095de7663
steel-bucket <depnra1@gmail.com>
2024-05-27 00:14:13 +0530

    server stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,24 @@
-import {db} from "~/server/db";
+import { db } from '~/server/db'
 
-export const dynamic = "force-dynamic";
-
-const mockUrls = [
-    "https://utfs.io/f/f476978c-039c-47b8-b55d-a2ac24c5cc58-1wb414.avif",
-    "https://utfs.io/f/d907478a-0e04-4e3a-8632-351da21c1dc8-bkvfhx.avif",
-    "https://utfs.io/f/f0afd3bc-3355-44ad-9b29-e14cc5aaf44b-o0xo9b.jpg",
-    "https://utfs.io/f/6f28ec06-d622-4d80-92c9-309c0ffa9b66-vto92j.avif"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url
-}))
+export const dynamic = 'force-dynamic'
 
 
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
-    console.log(posts);
+    const posts = await db.query.posts.findMany()
 
     return (
         <main className="">
             <div className="flex flex-wrap gap-3">
-                {posts.map((post) => (
-                    <div key={post.id} className="w-48">
-                        {post.name}
-                    </div>
-                ))}
+
                 {
-                    [...mockImages, ...mockImages, ...mockImages,].map((image, index) => (
-                        <div key={image.id + "-" + index} className="w-48">
-                            <img src={image.url}/>
+                    [...posts, ...posts, ...posts].map((image, index) => (
+                        <div key={image.id + '-' + index} className="w-48">
+                            <img src={image.url} />
+                            <div>{image.name}</div>
                         </div>
                     ))
                 }
             </div>
         </main>
-    );
+    )
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,14 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import {sql} from "drizzle-orm";
+import { sql } from 'drizzle-orm'
 import {
     index,
     pgTableCreator,
     serial,
     timestamp,
     varchar,
-} from "drizzle-orm/pg-core";
+} from 'drizzle-orm/pg-core'
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,19 +16,20 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `todoagain_${name}`);
+export const createTable = pgTableCreator((name) => `todoagain_${name}`)
 
 export const posts = createTable(
-    "post",
+    'post',
     {
-        id: serial("id").primaryKey(),
-        name: varchar("name", {length: 256}),
-        createdAt: timestamp("created_at", {withTimezone: true})
+        id: serial('id').primaryKey(),
+        name: varchar('name', { length: 256 }).notNull(),
+        url: varchar('url', { length: 1024 }).notNull(),
+        createdAt: timestamp('created_at', { withTimezone: true })
             .default(sql`CURRENT_TIMESTAMP`)
             .notNull(),
-        updatedAt: timestamp("updatedAt", {withTimezone: true}),
+        updatedAt: timestamp('updatedAt', { withTimezone: true }),
     },
     (example) => ({
-        nameIndex: index("name_idx").on(example.name),
-    })
-);
+        nameIndex: index('name_idx').on(example.name),
+    }),
+)
